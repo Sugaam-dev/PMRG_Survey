3a1143c262e357eb75718c6905ed4336a8a681bf
Sugaam-dev <pamirnayak6@gmail.com>
2026-01-10 10:30:52 +0530

    first

---
+++ application/extensions/AdminFooter/views/footer.php
@@ -0,0 +1,4 @@
+<div class='footer'>
+  
+    <div class='subtitle'><a class='subtitle' title='<?php eT("Visit our website!"); ?>' href='https://www.sugaam.in/' target='_blank'>PMRGSOLUTION Survey</a><br /><?php echo $versiontitle . " " . $versionnumber . $buildtext; ?></div>
+</div>
---
+++ application/views/layouts/partial_modals/modal_header.php
@@ -0,0 +1,8 @@
+<?php
+/**@var $modalTitle string  */
+?>
+
+<div class="modal-header">
+    <h2 class="h1 modal-title"><?php echo $modalTitle; ?></h2>
+    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+</div>
